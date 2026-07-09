ae33cb25385d1f2bcf82a9e85eb490da177f09a7
Sakuru Geethika <geethikasakuru@gmail.com>
2026-07-09 12:51:37 +0530

    Add files via upload

---
+++ Spring data JPA with Spring boot,hibernate/orm-learn/src/test/java/com/cogn/OrmLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
